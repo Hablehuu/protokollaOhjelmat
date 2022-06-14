fa6cf931b42e356a21443f22dae666aa547ef800
Hablehuu <73171541+Hablehuu@users.noreply.github.com>
2022-06-14 17:15:29 +0300

    Add files via upload

---
+++ UDPasiakas/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+public class httpAsiakasLuokka
+{
+    /// <summary>
+    /// UDP protokollaa käyttävä chätti asiakas
+    /// </summary>
+    public static void Main()
+    {
+        
+        Socket soketti = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        IPEndPoint iep = null;
+        string viesti = "";
+        byte[] rec = new byte[3000];
+        int paljon = 0;
+        Console.Write("kirjoita osoite johon yhdistetään:");
+        string syote = Console.ReadLine();
+        Console.Write("kirjoita portti:");
+        string portti = Console.ReadLine();
+        IPAddress[] addresses = Dns.GetHostAddresses(syote);
+        IPAddress ip = (IPAddress)addresses.GetValue(1);
+        iep = new IPEndPoint(ip, int.Parse(portti));
+        EndPoint senderRemote = (EndPoint)iep;
+        soketti.ReceiveTimeout = 1000;
+        byte[] viestii = System.Text.Encoding.ASCII.GetBytes(viesti);
+        while (true)
+        {
+            try
+            {
+                
+                soketti.SendTo(viestii = System.Text.Encoding.ASCII.GetBytes(viesti = Console.ReadLine()), senderRemote);
+                if (viesti.Equals("lopeta"))
+                {
+                    soketti.Close();
+                } 
+            }catch
+            {
+                continue;
+            }
+            try
+            {
+                
+                paljon = soketti.Receive(rec);
+                string vastaus = System.Text.Encoding.ASCII.GetString(rec, 0, paljon);
+                Console.WriteLine(vastaus);
+                
+            }catch
+            {
+                continue;
+            }
+        }
+        //soketti.Close();
+
+    }
+
+
+    
+}
---
+++ UDPpalvelin/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+
+/// <summary>
+/// UDP protokollaa käyttävä viesti palvelin joka toimii soketeilla.
+/// palvelin tallentaa käyttäjät ja toimii viestin välittäjänä
+///
+/// </summary>
+namespace UDPPalvelin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket palvelin = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint iep = new IPEndPoint(IPAddress.Any, 25000);
+            palvelin.Bind(iep);
+            byte[] rec = new byte[3000];
+            int paljon = 0;
+            IPEndPoint iap = new IPEndPoint(IPAddress.Any, 0);
+            EndPoint senderRemote = (EndPoint)iap;
+            EndPoint[] kayttajatIP = new EndPoint[20];
+            int maara = 0;
+            string viesti = "";
+            while (true)
+            {
+
+                paljon = palvelin.ReceiveFrom(rec, ref senderRemote);
+                if (!kayttajatIP.Contains(senderRemote))
+                {
+                    kayttajatIP[maara] = senderRemote;
+                    maara++;
+                }
+                Console.WriteLine("yhteys osoitteesta: " + senderRemote.ToString());
+                viesti = System.Text.Encoding.ASCII.GetString(rec, 0, paljon);
+                Console.WriteLine(viesti);
+                if (viesti.Contains(";"))
+                {
+
+                    byte[] viestii = System.Text.Encoding.ASCII.GetBytes(viesti);
+                    for (int i = 0; i < maara; i++)
+                    {
+                        palvelin.SendTo(viestii, kayttajatIP[i]);
+                    }
+                }
+                else
+                {
+                    string virhe = "viesti oli väärässä moudossa";
+                    byte[] viestii = System.Text.Encoding.ASCII.GetBytes(virhe);
+                    palvelin.SendTo(viestii, senderRemote);
+                }
+
+
+            }
+            //Console.ReadKey();
+            //palvelin.Close();
+        }
+
+
+    }
+
+
+}
